3c2f81550ddd937f597f697a02ea8bbf1aad0a32
Raul Cuevas Gorocica <raul8527@gmail.com>
2013-03-04 19:39:12 -0600

    Se agrega función de envio de correo
    Se agrega el archivo pages/includes/enviacorreo.php para habilitar la
    forma de reservación y se modifica el archivo pages/includes/form.php
    para agregar validación de campos con js y habilitar el funcionamiento
    de la forma.

---
+++ pages/includes/enviacorreo.php
@@ -0,0 +1,49 @@
+<?php
+	//Forma para el envio de correo al cliente
+	$nombre = $_GET['txtNombre'];
+	$ciudad = $_GET['txtCiudad'];
+	$estado = $_GET['txtEstado'];
+	$cp = $_GET['txtCP'];
+	$pais = $_GET['txtPais'];
+	$tel = $_GET['txtTel'];
+	$email = $_GET['txtEmail'];
+	$hotel = $_GET['txtHotel'];
+	$numPersonas = $_GET['txtPersonas'];
+	$fechaLlegada = $_GET['txtFechaLlegada'];
+	$fechaSalida = $_GET['txtFechaSalida'];
+	$tipoHabitacion1 = $_GET['txtTipoHabitacion1'];
+	$numCuartos1 = $_GET['txtNumCuartos1'];
+	$tipoHabitacion2 = $_GET['txtTipoHabitacion2'];
+	$numCuartos2 = $_GET['txtNumCuartos2'];
+	$tipoHabitacion3 = $_GET['txtTipoHabitacion3'];
+	$numCuartos3 = $_GET['txtNumCuartos3'];
+	$comentarios = $_GET['txtComentarios'];
+	
+	$mensaje = "Informacion de Reservacion de Hoteles Prado del Rio \r\n -------------------------------------------------------------- \r\n" .  " Nombre: " . $nombre . "\r\n" .
+	" Ciudad: " . $ciudad . "\r\n" .
+	" Estado: " . $estado . "\r\n" .
+	" Codigo Postal: " . $cp . "\r\n" .
+	" Pais: " . $pais . "\r\n" .
+	" Telefono: " . $tel . "\r\n" .
+	" Correo electronico: " .  $email . "\r\n" .
+	"-------------------------------------------------------------- \r\n" .
+	" Hotel: " . $hotel . "\r\n" .
+	" Numero de personas: " . $numPersonas . "\r\n" .
+	" Fecha de llegada: " . $fechaLlegada . "\r\n" .
+	" Fecha de salida: " . $fechaSalida . "\r\n" .
+	" Tipo de habitacion: " . $tipoHabitacion1 . "\r\n" .
+	" Numero de habitaciones: " . $numCuartos1 . "\r\n" .
+	"-------------------------------------------------------------- \r\n" .
+	" Tipo de habitacion (Opcional): " . $tipoHabitacion2 . "\r\n" .
+	" Numero de habitaciones (Opcional): " . $numCuartos2 . "\r\n" .
+	" Tipo de habitacion (Opcional): " . $tipoHabitacion3 . "\r\n" .
+	" Numero de habitaciones (Opcional): " . $numCuartos3 . "\r\n" .
+	"-------------------------------------------------------------- \r\n" .
+	" Comentarios: " . $comentarios . "\r\n-------------------------------------------------------------- \r\n" . "Enviado desde www.pradodelrio.com" ;
+	
+	if (mail("reservaciones@pradodelrio.com", "Informacion de Reservacion de Hoteles Prado del Rio", $mensaje)) {
+		print "Tu correo de reservaci&oacute;n se ha enviado correctamente, gracias.";
+	} else {
+		print "Ha ocurrido un problema y no pudimos enviar el correo de reservaci&oacute;n, por favor, intentalo m&aacute;s tarde.";
+	}
+?>
--- pages/includes/form.php
+++ pages/includes/form.php
@@ -1,127 +1,168 @@
-<form id="form">
+<script>
+function enviaMail(){
+	if (document.getElementById('txtNombre').value == '' || document.getElementById('txtCiudad').value == '' || document.getElementById('txtEstado').value == '0' || document.getElementById('txtPais').value == '' || document.getElementById('txtTel').value == '' || document.getElementById('txtEmail').value == '' ) {
+		alert("Por favor, ingresa todos los campos marcados como obligatorios, puedes identificarlos por el signo: *");
+	} else {
+	$.get(
+		'../includes/enviacorreo.php',
+		{
+			txtNombre : document.getElementById('txtNombre').value,
+			txtCiudad : document.getElementById('txtCiudad').value,
+			txtEstado : document.getElementById('txtEstado').value,
+			txtCP : document.getElementById('txtCP').value,	
+			txtPais : document.getElementById('txtPais').value,
+			txtTel : document.getElementById('txtTel').value,
+			txtEmail : document.getElementById('txtEmail').value,
+			txtHotel : document.getElementById('txtHotel').value,
+			txtPersonas : document.getElementById('txtPersonas').value,
+			txtFechaLlegada : document.getElementById('txtFechaLlegada').value,
+			txtFechaSalida : document.getElementById('txtFechaSalida').value,
+			txtTipoHabitacion1 : document.getElementById('txtTipoHabitacion1').value,
+			txtNumCuartos1 : document.getElementById('txtNumCuartos1').value,
+			txtTipoHabitacion2 : document.getElementById('txtTipoHabitacion2').value,
+			txtNumCuartos2 : document.getElementById('txtNumCuartos2').value,
+			txtTipoHabitacion3 : document.getElementById('txtTipoHabitacion3').value,
+			txtNumCuartos3 : document.getElementById('txtNumCuartos3').value,
+			txtComentarios : document.getElementById('txtComentarios').value
+		},
+		function success(data) {
+			$(".msjCorreoEnviado").html(data);
+			document.getElementById("ocultaForma").style.display = 'none';
+		}
+	);
+	}
+}
+</script>
+
+<div class="msjCorreoEnviado"></div>
+<div id="ocultaForma">
+<form id="form" name="form" method="post" >
 	
 	<div class="row">
 		<label>Nombre/Name*</label>
-		<input type="text">
+		<input type="text" name="txtNombre" id="txtNombre" >
 	</div>
 	
 	<div class="row">
 		<label>Ciudad/City*</label>
-		<input type="text">
+		<input type="text" name="txtCiudad" id="txtCiudad" >
 	</div>
 	
 	<div class="row">
 		<label>Estado/State*</label>
-		<select><option>Seleccione su estado</option>
-		<option>Fuera de México/Outside Mexico</option>
-		<option>Aguascalientes</option>
-		<option>Baja California</option>
-		<option>Baja California Sur</option>
-		<option>Campeche</option>
-		<option>Coahuila</option>
-		<option>Colima</option>
-		<option>Chiapas</option>
-		<option>Chihuahua</option>
-		<option>Distrito Federal</option>
-		<option>Durango</option>
-		<option>Guanajuato</option>
-		<option>Guerrero</option>
-		<option>Hidalgo</option>
-		<option>Jalisco</option>
-		<option>Estado de México</option>
-		<option>Michoacán</option>
-		<option>Morelos</option>
-		<option>Nayarit</option>
-		<option>Nuevo León</option>
-		<option>Oaxaca</option>
-		<option>Puebla</option>
-		<option>Querétaro</option>
-		<option>Quintana Roo</option>
-		<option>San Luis Potosí</option>
-		<option>Sinaloa</option>
-		<option>Sonora</option>
-		<option>Tabasco</option>
-		<option>Tamaulipas</option>
-		<option>Tlaxcala</option>
-		<option>Veracruz</option>
-		<option>Yucatán</option>
-		<option>Zacatecas</option>
+		<select name="txtEstado" id="txtEstado">
+		<option value="0" >Seleccione su estado</option>
+		<option value="Fuera de Mexico">Fuera de México/Outside Mexico</option>
+		<option value="Aguascalientes" >Aguascalientes</option>
+		<option value="Baja California" >Baja California</option>
+		<option value="Baja California Sur" >Baja California Sur</option>
+		<option value="Campeche" >Campeche</option>
+		<option value="Coahuila" >Coahuila</option>
+		<option value="Colima" >Colima</option>
+		<option value="Chiapas" >Chiapas</option>
+		<option value="Chihuahua" >Chihuahua</option>
+		<option value="Distrito Federal" >Distrito Federal</option>
+		<option value="Durango" >Durango</option>
+		<option value="Guanajuato" >Guanajuato</option>
+		<option value="Guerrero" >Guerrero</option>
+		<option value="Hidalgo" >Hidalgo</option>
+		<option value="Jalisco" >Jalisco</option>
+		<option value="Estado de Mexico" >Estado de México</option>
+		<option value="Michoacan" >Michoacán</option>
+		<option value="Morelos" >Morelos</option>
+		<option value="Nayarit" >Nayarit</option>
+		<option value="Nuevo Leon" >Nuevo León</option>
+		<option value="Oaxaca" >Oaxaca</option>
+		<option value="Puebla" >Puebla</option>
+		<option value="Queretaro" >Querétaro</option>
+		<option value="Quintana Roo" >Quintana Roo</option>
+		<option value="San Luis Potosi" >San Luis Potosí</option>
+		<option value="Sinaloa" >Sinaloa</option>
+		<option value="Sonora" >Sonora</option>
+		<option value="Tabasco" >Tabasco</option>
+		<option value="Tamaulipas" >Tamaulipas</option>
+		<option value="Tlaxcala" >Tlaxcala</option>
+		<option value="Veracruz" >Veracruz</option>
+		<option value="Yucatan" >Yucatán</option>
+		<option value="Zacatecas" >Zacatecas</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>CP/Zip Code</label>
-		<input type="text">
+		<input type="text" name="txtCP" id="txtCP" >
 	</div>
 	
 	<div class="row">
 		<label>País/Country*</label>
-		<input type="text">
+		<input type="text" name="txtPais" id="txtPais" >
 	</div>
 	
 	<div class="row">
 		<label>Teléfono/Phone*</label>
-		<input type="text">
+		<input type="text" name="txtTel" id="txtTel" >
 	</div>
 	
 	<div class="row">
 		<label>Email*</label>
-		<input type="text">
+		<input type="text" name="txtEmail" id="txtEmail" >
 	</div>
 	
 	<h4>Seleccione el hotel donde quiere hacer la reservación:</h4>
 	
 	<div class="row">
 		<label>Hotel</label>
-		<select>
-		<option label="Seleccione su Hotel">Seleccione el Hotel</option>
-		<option label="Hotel Mesón del Alférez Xalapa">Hotel Mesón del Alférez Xalapa</option>
-		<option label="Hotel Posada del Cafeto">Hotel Posada del Cafeto</option>
-		<option label="Hotel Mesón del Alférez Coatepec">Hotel Mesón del Alférez Coatepec</option>
-		<option label="Hotel Balcón del Alférez">Hotel Balcón del Alférez</option>
-		<option label="Hostal de Niebla">Hostal de Niebla</option>
+		<select name="txtHotel" id="txtHotel" >
+		<option value="Seleccione su Hotel">Seleccione el Hotel</option>
+		<option value="Hotel Mesón del Alférez Xalapa">Hotel Mesón del Alférez Xalapa</option>
+		<option value="Hotel Posada del Cafeto">Hotel Posada del Cafeto</option>
+		<option value="Hotel Mesón del Alférez Coatepec">Hotel Mesón del Alférez Coatepec</option>
+		<option value="Hotel Balcón del Alférez">Hotel Balcón del Alférez</option>
+		<option value="Hostal de Niebla">Hostal de Niebla</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>Personas / Number of travelers </label>
-		<select>
-		<option label="1">1</option>
-		<option label="2">2</option>
-		<option label="3">3</option>
-		<option label="4">4</option>
-		<option label="4">5</option>
-		<option label="4">6</option>
+		<select name="txtPersonas" id="txtPersonas">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>Fecha de Llegada / Arrival date</label>
-		<input type="text"><small>[dd/mm/año] [dd/mm/year]</small>
+		<input type="text" name="txtFechaLlegada" id="txtFechaLlegada" ><small>[dd/mm/año] [dd/mm/year]</small>
 	</div>
 	
 	<div class="row">
 		<label>Fecha de Sálida / Check out date</label>
-		<input type="text"><small>[dd/mm/año] [dd/mm/year]</small>
+		<input type="text" name="txtFechaSalida" id="txtFechaSalida" ><small>[dd/mm/año] [dd/mm/year]</small>
 	</div>
 	
 	<div class="row">
 		<label>Habitación / Room request</label>
-		<select>
-		<option label="Sencilla">Sencilla</option>
-		<option label="Doble">Doble</option>
-		<option label="Junior Suite">Junior Suite</option>
-		<option label="Suite">Suite</option>
+		<select name="txtTipoHabitacion1" id="txtTipoHabitacion1" >
+		<option value="Sencilla">Sencilla</option>
+		<option value="Doble">Doble</option>
+		<option value="Junior Suite">Junior Suite</option>
+		<option value="Suite">Suite</option>
+		<option value="Privada H. Niebla">Privada H. Niebla</option>
+		<option value="Compartida H. Niebla">Compartida H. Niebla</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>Cantidad/Number of rooms</label>
-		<select>
-		<option label="1">1</option>
-		<option label="2">2</option>
-		<option label="3">3</option>
-		<option label="4">4</option>
+		<select name="txtNumCuartos1" id="txtNumCuartos1" >
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
 		</select>
 	</div>
 	<div class="row">
@@ -129,56 +170,60 @@
 	</div>
 	<div class="row">
 		<label>Habitación / Room request</label>
-		<select>
-		<option label="--">--</option>
-		<option label="Sencilla">Sencilla</option>
-		<option label="Doble">Doble</option>
-		<option label="Junior Suite">Junior Suite</option>
-		<option label="Suite">Suite</option>
+		<select name="txtTipoHabitacion2" id="txtTipoHabitacion2" >
+		<option value="--">--</option>
+		<option value="Sencilla">Sencilla</option>
+		<option value="Doble">Doble</option>
+		<option value="Junior Suite">Junior Suite</option>
+		<option value="Suite">Suite</option>
+		<option value="Privada H. Niebla">Privada H. Niebla</option>
+		<option value="Compartida H. Niebla">Compartida H. Niebla</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>Cantidad/Number of rooms</label>
-		<select>
-		<option label="--">--</option>
-		<option label="1">1</option>
-		<option label="2">2</option>
-		<option label="3">3</option>
-		<option label="4">4</option>
+		<select name="txtNumCuartos2" id="txtNumCuartos2" >
+		<option value="--">--</option>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>Habitación / Room request</label>
-		<select>
-		<option label="--">--</option>
-		<option label="Sencilla">Sencilla</option>
-		<option label="Doble">Doble</option>
-		<option label="Junior Suite">Junior Suite</option>
-		<option label="Suite">Suite</option>
+		<select name="txtTipoHabitacion3" id="txtTipoHabitacion3" >
+		<option value="--">--</option>
+		<option value="Sencilla">Sencilla</option>
+		<option value="Doble">Doble</option>
+		<option value="Junior Suite">Junior Suite</option>
+		<option value="Suite">Suite</option>
+		<option value="Privada H. Niebla">Privada H. Niebla</option>
+		<option value="Compartida H. Niebla">Compartida H. Niebla</option>
 		</select>
 	</div>
 	
 	<div class="row">
 		<label>Cantidad/Number of rooms</label>
-		<select>
-		<option label="--">--</option>
-		<option label="1">1</option>
-		<option label="2">2</option>
-		<option label="3">3</option>
-		<option label="4">4</option>
+		<select name="txtNumCuartos3" id="txtNumCuartos3" >
+		<option value="--">--</option>
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
 		</select>
 	</div>
 
 	
 	<div class="row">
 		<label>Comentarios</label>
-		<textarea></textarea>
+		<textarea name="txtComentarios" id="txtComentarios" ></textarea>
 	</div>
 	
 	<div class="row">
-		<input value="Enviar/Send" type="button">
+		<input value="Enviar/Send" type="button" onclick="enviaMail();">
 	</div>
 	
 	<div class="row">
@@ -186,7 +231,7 @@
 	</div>
 	
 </form>
-
+</div><!-- Fin del div que oculta la forma -->
 
 
 
